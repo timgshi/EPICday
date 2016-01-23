d8d64ff942344ea8ed93796a9667facf3574b946
Tim Shi <timgshi@gmail.com>
2016-01-23 15:04:15 -0800

    first commit

---
+++ EPICday/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EPICday
+//
+//  Created by Tim Shi on 1/23/16.
+//  Copyright © 2016 Epicurrence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
