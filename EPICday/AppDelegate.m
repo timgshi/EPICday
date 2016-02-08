516b6c211c53f175567d6f40c77da8d27184f1ba
Tim Shi <tim@bluebottlecoffee.com>
2016-02-08 14:42:55 -0700

    adding futura

--- EPICday/AppDelegate.m
+++ EPICday/AppDelegate.m
@@ -31,9 +31,8 @@
     AWSServiceConfiguration *configuration = [[AWSServiceConfiguration alloc] initWithRegion:AWSRegionUSEast1 credentialsProvider:credentialsProvider];
     
     [AWSServiceManager defaultServiceManager].defaultServiceConfiguration = configuration;
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        
-    });
+    NSLog(@"%@", [UIFont familyNames]);
+    NSLog(@"%@", [UIFont fontNamesForFamilyName:@"Futura"]);
     return YES;
 }
 
--- EPICday/UIFont+EPIC.m
+++ EPICday/UIFont+EPIC.m
@@ -11,14 +11,15 @@
 @implementation UIFont (EPIC)
 
 + (instancetype)epicDefaultFontOfSize:(CGFloat)size {
-    return [self systemFontOfSize:size];
+    return [self fontWithName:@"FuturaBT-Book" size:size];
 }
 
 + (instancetype)epicLightFontOfSize:(CGFloat)size {
-    return [self systemFontOfSize:size weight:UIFontWeightLight];
+    return [self fontWithName:@"FuturaBT-Book" size:size];
 }
+
 + (instancetype)epicBoldFontOfSize:(CGFloat)size {
-    return [self boldSystemFontOfSize:size];
+    return [self fontWithName:@"FuturaBT-Heavy" size:size];
 }
 
 @end
